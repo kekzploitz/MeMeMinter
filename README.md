2a5507f4b797d36cf12e550dcfbaca4f3d51e324
Kekzploit <kekzploit@proton.me>
2023-01-08 04:43:09 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# MeMeMinter
+<div align="center">
+  <img title="" src="assets/images/meme_minter.png" alt="meme minter logo" data-align="center" width="400">
+  </br>
+
+<p><i>MeMe Minter brings the power of AI image generation to your MeMe warfare efforts. Fund your war chest and get creating! created by <a href="https://twitter.com/Kekzploit/">@kekzploit</a></i></p>
+  <hr>
+</div>
+
+# AI Image Generation by OpenAI
+
+MeMe Minter uses [OpenAI's DALL·E 2 AI system](https://openai.com/dall-e-2/) to generate images. DALL·E 2 can create realistic images and art from a description in natural language.
---
+++ cmd/meme-minter/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/spf13/viper"
+
+	tele "gopkg.in/telebot.v3"
+	"gopkg.in/telebot.v3/middleware"
+)
+
+func main() {
+	// initiate environment variable config file
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("../../configs/")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	tgToken := viper.Get("TELEGRAM.APIKEY").(string)
+
+	pref := tele.Settings{
+		Token:  tgToken,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Global-scoped middleware:
+	b.Use(middleware.Logger())
+	b.Use(middleware.AutoRespond())
+
+	// Group-scoped middleware:
+	adminOnly := b.Group()
+
+	admins := []int64{5578680936} // kekzploit user ID
+	adminOnly.Use(middleware.Whitelist(admins...))
+
+	adminOnly.Handle("/totals", func(c tele.Context) error {
+
+		var (
+		// payload = c.Message().Payload
+		// user = c.Sender()
+		// text = c.Text()
+		)
+
+		msg := fmt.Sprintln("admin only")
+		return c.Send(msg)
+	})
+
+	b.Handle(tele.OnUserJoined, func(c tele.Context) error {
+
+		var (
+			user = c.Sender()
+		)
+
+		msg := fmt.Sprintf("Welcome %s", user.FirstName)
+		return c.Send(msg)
+	})
+
+	b.Handle("/start", func(c tele.Context) error {
+
+		var (
+		// payload = c.Message().Payload
+		// user = c.Sender()
+		// text = c.Text()
+		)
+
+		msg := fmt.Sprintln("Hello, world!")
+		return c.Send(msg)
+	})
+
+	b.Start()
+}
